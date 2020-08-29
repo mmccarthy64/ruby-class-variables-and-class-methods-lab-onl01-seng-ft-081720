cfde6fb67d7ce5600555fa0adc27a61623352feb
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-29 21:36:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
     @@genres.uniq
   end
   
-  def genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
@@ -40,7 +40,7 @@ class Song
     @@artists.uniq
   end
   
-  def artist_count
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
